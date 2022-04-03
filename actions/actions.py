369c691cbc0d19c1b22c1bbc29136b899b105266
Alar31000 <lsfezza@gmail.com>
2022-04-03 17:23:17 -0400

    Sending Email capabilities

--- actions/actions.py
+++ actions/actions.py
@@ -8,9 +8,12 @@
 # This is a simple example for a custom action which utters "Hello World!"
 import os
 import time
+import smtplib
 from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 class ActionSaveConvo(Action):
     def name(self) -> Text:
@@ -52,3 +55,82 @@ class ActionSaveConvo(Action):
                 file.write(chat_data)
 
         return []
+
+
+class ActionSubmit(Action):
+    def name(self) -> Text:
+        return "action_submit"
+
+    def run(
+        self,
+        dispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        SendEmail(
+            tracker.get_slot("email"),
+            tracker.get_slot("subject"),
+            tracker.get_slot("message")
+        )
+        dispatcher.utter_message("Merci d'avoir fourni les détails. Nous vous avons envoyé un courriel de confirmation à {}".format(tracker.get_slot("email")))
+        return []
+
+def SendEmail(toaddr,subject,message):
+    fromaddr = "chatbotuqotest@gmail.com"
+    # instance of MIMEMultipart
+    msg = MIMEMultipart()
+
+    # storing the senders email address
+    msg['From'] = fromaddr
+
+    # storing the receivers email address
+    msg['To'] = toaddr
+
+    # storing the subject
+    msg['Subject'] = subject
+
+    # string to store the body of the mail
+    body = message
+
+    # attach the body with the msg instance
+    msg.attach(MIMEText(body, 'plain'))
+
+    # open the file to be sent
+    # filename = "/home/ashish/Downloads/webinar_rasa2_0.png"
+    # attachment = open(filename, "rb")
+    #
+    # # instance of MIMEBase and named as p
+    # p = MIMEBase('application', 'octet-stream')
+    #
+    # # To change the payload into encoded form
+    # p.set_payload((attachment).read())
+    #
+    # # encode into base64
+    # encoders.encode_base64(p)
+    #
+    # p.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+    #
+    # # attach the instance 'p' to instance 'msg'
+    # msg.attach(p)
+
+    # creates SMTP session
+    s = smtplib.SMTP('smtp.gmail.com', 587)
+
+    # start TLS for security
+    s.starttls()
+
+
+    # Authentication
+    try:
+        s.login(fromaddr, "password")
+
+        # Converts the Multipart msg into a string
+        text = msg.as_string()
+
+        # sending the mail
+        s.sendmail(fromaddr, toaddr, text)
+    except:
+        print("Une erreur s'est produite lors de l'envoi du courriel. Veuillez réessayer ultérieurement.")
+    finally:
+        # terminating the session
+        s.quit()
